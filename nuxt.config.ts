7211075385caaf6a944bc58e42296a63412ccf4a
Ulil Albab <ulil.alb21@gmail.com>
2024-08-22 00:20:31 +0700

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  compatibilityDate: "2024-08-22"
+})

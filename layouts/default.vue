87d7a7223620682c13ae8db7a7410cfb354e4160
Ulil Albab <ulil.alb21@gmail.com>
2024-08-23 09:08:47 +0700

    desktop view

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="font-[Nunito] text-woodsmoke-950 dark:text-white">
-    <nav
-      class="px-6 py-8 flex flex-auto justify-between items-center bg-white dark:bg-oxford-blue-900"
-    >
-      <h2 class="font-bold tracking-wide">Where in the world?</h2>
+    <nav class="bg-white dark:bg-oxford-blue-900">
       <div
-        class="flex items-center text-sm font-semibold cursor-pointer"
-        @click="isDark = !isDark"
+        class="px-6 py-8 md:container md:mx-auto flex flex-auto justify-between items-center"
       >
-        <UIcon
-          :name="`i-heroicons-moon${isDark ? '-solid' : ''}`"
-          class="w-4 h-4 mr-2"
-        />
-        Dark Mode
+        <h2 class="font-bold tracking-wide">Where in the world?</h2>
+        <div
+          class="flex items-center text-sm font-semibold cursor-pointer"
+          @click="isDark = !isDark"
+        >
+          <UIcon
+            :name="`i-heroicons-moon${isDark ? '-solid' : ''}`"
+            class="w-4 h-4 mr-2"
+          />
+          Dark Mode
+        </div>
       </div>
     </nav>
     <div class="bg-gray-50 dark:bg-shark-950">
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="p-6">
-    <div class="flex flex-col items-center">
+  <div class="md:container md:mx-auto p-6">
+    <div class="md:flex md:justify-between md:items-center">
+      <UInput
+        class="mb-10 md:w-1/3"
+        icon="i-heroicons-magnifying-glass-20-solid"
+        size="lg"
+        color="white"
+        :trailing="false"
+        placeholder="Search for a country..."
+      />
+      <UDropdown :items="regions" class="mb-8">
+        <UButton
+          size="lg"
+          color="white"
+          label="Filter By Region"
+          trailing-icon="i-heroicons-chevron-down-20-solid"
+        />
+      </UDropdown>
+    </div>
+    <div
+      class="flex flex-col items-center md:flex-wrap md:flex-row md:justify-between"
+    >
       <div
         v-for="country in data"
         :key="country.code"
-        class="w-[300px] h-[380px] box-shadow-md bg-white dark:bg-oxford-blue-900 mb-10 box-shadow-lg rounded-lg overflow-hidden"
+        class="w-[300px] h-[380px] box-shadow-md bg-white dark:bg-oxford-blue-900 mb-10 box-shadow-lg rounded-lg overflow-hidden cursor-pointer"
       >
         <NuxtImg class="w-full h-[200px] object-cover" :src="country.flag" />
         <div class="p-7">
@@ -36,7 +56,25 @@ const formatter = new Intl.NumberFormat('en-US');
 definePageMeta({
   title: 'Where in the world?',
 });
-
+const regions = [
+  [
+    {
+      label: 'Africa',
+    },
+    {
+      label: 'America',
+    },
+    {
+      label: 'Asia',
+    },
+    {
+      label: 'Europe',
+    },
+    {
+      label: 'Oceania',
+    },
+  ],
+];
 const formatNumber = (num: number) => formatter.format(num);
 </script>
 <style></style>
